e6c3b78636a6b539d5a39101040dae2bf8b04301
mekarpeles <michael.karpeles@gmail.com>
2013-08-20 23:57:14 +0000

    refactored flow, added goalp to select/filter important events

--- minotaur.py
+++ minotaur.py
@@ -42,9 +42,13 @@ def tail( f, window=20 ):
         block -= 1
     return ''.join(data[::-1]).splitlines()[-window:]
 
-def nginxmon(logfile):
-    """Returns the last nginx error from the file"""
-    with open(logfile) as log:        
+def nginxmon_goalp(content):
+    return False if any(["failed (2: No such file or directory)" in content]) \
+        else True
+
+def nginxmon_parser(logfile):
+    """Returns the last entry from the nginx error.log file"""
+    with open(logfile) as log:
         contents = tail(log)
         line = contents.pop()
         tmp = [line]
@@ -53,23 +57,32 @@ def nginxmon(logfile):
             tmp.append(line)
         return '\n'.join(tmp[::-1])
                 
-def event(notifier, mailserver, parser=None):
+def callback(notifier, parser=None, goalp=None):
+    content = parser() if parser else ''
+    if goalp and not goalp(content):
+        print("Skipping: %s" % content)
+        return
+    mailserver = MailServer(**email_creds)
     t = datetime.datetime.now().ctime()
     subject = "ERROR alert: (%s) at %s" % (service['name'], t)
-    msg = parser() if parser else ''
     mailserver.sendmail(sender=mailserver.email, recipients=mailserver.email,
-                        subject=subject, msg=msg, fmt='plain')
+                        subject=subject, msg=content, fmt='plain')
 
-if __name__ == "__main__":
+def patrol(filename, event, success=callback, goalp=lambda *args, **kwargs: True):
     monitor = pyinotify.WatchManager()
+    monitor.add_watch(filename, event)
     notifier = pyinotify.Notifier(monitor)
-    monitor.add_watch(service['logfile'], pyinotify.IN_CLOSE_WRITE)
-    parser = functools.partial(nginxmon, service['logfile'])
-    callback = functools.partial(event, mailserver=MailServer(**email_creds),
-                                 parser=parser)
-    while True:
+    
+    while goalp(notifier):
         try:
             notifier.loop(daemonize=False, callback=callback,
                           pid_file='/tmp/pyinotify.pid', stdout='/tmp/stdout.txt')
         except pyinotify.NotifierError, err:
             print >> sys.stderr, err
+
+if __name__ == "__main__":
+    filename = service['logfile']
+    event = getattr(pyinotify, service['event'], 'IN_MODIFY')
+    parser = functools.partial(nginxmon_parser, filename)
+    callback = functools.partial(callback, parser=parser, goalp=nginxmon_goalp)
+    patrol(filename, event, success=callback)
