abdb015d4264a77fa92a51ba26f7788cc7c53dd5
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-08-20 06:59:24 -0700

    configparser

---
+++ configs/config.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""
+    config.py
+    ~~~~~~~~~
+
+    This module is the middle man for handling/consolidating
+    configurations for Minotaur.
+
+    :copyright: (c) 2012 by Mek, Hackerlist
+    :license: BSD, see LICENSE for more details.
+"""
+
+import ConfigParser
+import os
+import types
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+def getdef(self, section, option, default_value, _type=None):
+    try:
+        v = self.get(section, option)
+        return _type(v) if _type else v
+    except:
+        return default_value
+
+config = ConfigParser.ConfigParser()
+config.read('%s/minotaur.cfg' % path)
+config.getdef = types.MethodType(getdef, config)
+
+email_creds = {'email': config.getdef('mail', 'email', ''),
+               'passwd': config.getdef('mail', 'passwd', ''),
+               'host': config.getdef('mail', 'host', 'smtp.gmail.com'),
+               'port': config.getdef('mail', 'host', 587, int)
+               }
+
+service = {'name': config.getdef('service', 'name', ''),
+           'logfile': config.getdef('service', 'logfile', '')
+           }
